8de5a2c217335a3a93e845a9615d9928c2576538
Kurt Mueller <kmuel4@brockport.edu>
2023-04-03 15:30:31 -0400

    main card

--- src/components/Layout/MainCard/AboutTab.js
+++ src/components/Layout/MainCard/AboutTab.js
@@ -4,17 +4,14 @@ const AboutTab = () => {
   return (
     <Container>
       <h2>About</h2>
-      <Row>
-        <Col>
+      <p>
           Choose your favorite entree or snack from our broad selection of
           available menu items and enjoy a delicious meal on the go.
-        </Col>
-        <Col>
+        </p><p>
           Click to add items to your cart, pay with points or credit
-          card, and pick up in store! Check out the Coupons tab to take part in
-          the daily coupon giveaway.
-        </Col>
-      </Row>
+          card, and pick up in store! Check out the <strong>Coupons </strong>
+          tab to take part in the daily coupon giveaway.
+        </p>
     </Container>
   );
 };
